2831909888e9e5dba301dc9c8398dcedca67fac2
Breno Bernardes <breno.pbernardes@gmail.com>
2022-04-09 19:06:36 -0300

    iniciado server

--- src/pages/Coletas-disponiveis/coletasDisponiveis.js
+++ src/pages/Coletas-disponiveis/coletasDisponiveis.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 
 export default function coletasDisponiveis () {
+    let [colletcs, setCollects] = useState([])
+
+    useEffect(() => {
+        fetch("/api/collects")
+        .then((response) => response.json())
+        .then((json) => setCollects(json.collects))
+    }, [])
+
     return (
         <View>
             <Text>Coletas Disponiveis</Text>
+            
+            {colletcs.map((collect) => (
+                <Text key={collect.id}>{collect.situation}</Text>
+            ))}
         </View>
     )
 }
